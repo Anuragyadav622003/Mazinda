808ab885a4963b41174ecb0b5bb295809ac37d3c
Anurag yadav <anuragyadav622003@gmail.com>
2024-12-18 03:55:12 +0530

    change

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,9 +1,17 @@
-import React from 'react'
+// app/admin/page.js
+"use client"
+import AccountList from "./components/AccountList";
+import WalletControl from "./components/WalletControl";
+import AccountControl from "./components/AccountControl";
 
-function page() {
+export default function AdminPanel() {
   return (
-    <div>page</div>
-  )
+    <div className="container mx-auto p-6">
+      <h1 className="text-2xl font-bold">Admin Panel</h1>
+      <AccountList />
+      {/* Example usage for Wallet and Account Controls */}
+      <WalletControl walletId="12345" status="active" /> 
+      <AccountControl accountId="12345" /> 
+    </div>
+  );
 }
-
-export default page
